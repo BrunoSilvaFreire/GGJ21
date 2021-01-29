68d7ee6649d692859bee5f4705b66406ea9e0f6c
BrunoSilvaFreire <brunorbsf@gmail.com>
2021-01-29 00:36:24 -0300

    > Basic interaction works

--- Assets/Scripts/Traits/Knowledge/Knowledgeable.cs
+++ Assets/Scripts/Traits/Knowledge/Knowledgeable.cs
@@ -37,7 +37,7 @@ namespace GGJ.Traits.Knowledge {
             LookUp = 1 << 3,
             LookDown = 1 << 4,
             // Crouch = 1 << 5,
-            Interact = 1 << 6,
+            // Interact = 1 << 6,
             Attack = 1 << 7,
             WallJump = 1 << 8,
             DoubleJump = 1 << 9
