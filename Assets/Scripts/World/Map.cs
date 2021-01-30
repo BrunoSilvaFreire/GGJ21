ca90f0f8dcbe9cf122a859438a771a701ee4bacb
Ricardo Cicaida Novak <ricardonovak@yahoo.com.br>
2021-01-30 13:24:28 -0300

    -Dynamic map activation implemented

--- Assets/Scripts/World/Map.cs
+++ Assets/Scripts/World/Map.cs
@@ -1,35 +1,53 @@
 using System;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace World {
     public class Map : MonoBehaviour, ITiledMap {
 
-        [SerializeField] private List<string> m_adjacentScenes;
-        
-        public Vector2Int Coordinates { get; private set; }
-        
+        [SerializeField] private Vector2Int m_coordinates;
+        public Vector2Int Coordinates => m_coordinates;
+
+        private bool m_playerInside = false;
+
         public void Setup(MapData data) {
-            Coordinates = Vector2Int.zero;
+            m_coordinates = Vector2Int.zero;
             foreach (var property in data.properties) {
                 if (property.name.Equals("mapx")) {
-                    Coordinates += new Vector2Int(int.Parse(property.value), 0);
+                    m_coordinates += new Vector2Int(int.Parse(property.value), 0);
                 }
                 else if (property.name.Equals("mapy")) {
-                    Coordinates += new Vector2Int(0, int.Parse(property.value));
+                    m_coordinates += new Vector2Int(0, int.Parse(property.value));
                 }
             }
-            MapConnectionConfig.Instance.AddMap(SceneManager.GetActiveScene(), Coordinates);
             transform.position += new Vector3(Coordinates.x * data.width, Coordinates.y * -data.height);
         }
+
+        private void Awake() {
+            MapManager.Instance.AddMap(this);
+        }
+
+        public void Activate() {
+            gameObject.SetActive(true);
+        }
+
+        public void Deactivate() {
+            gameObject.SetActive(false);
+        }
+        
         
         public void OnEnterMap() {
+            if (m_playerInside) {
+                return;
+            }
+            m_playerInside = true;
             MapManager.Instance.SetActiveMap(this);
         }
 
         public void OnLeaveMap() {
-            
+            m_playerInside = false;
         }
     }
 }
--- Assets/Scripts/World/MapManager.cs
+++ Assets/Scripts/World/MapManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using Lunari.Tsuki.Graphs;
 using Lunari.Tsuki.Runtime.Singletons;
+using Sirenix.Utilities;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,33 +10,65 @@ namespace World {
     
     public class MapManager : Singleton<MapManager> {
 
-        private Dictionary<string, AsyncOperation> m_loadedScenes = new Dictionary<string, AsyncOperation>();
+        private Dictionary<Vector2Int, Map> m_maps = new Dictionary<Vector2Int, Map>();
 
-        [SerializeField] private string m_firstScene;
+        [SerializeField] private Map m_initialMap;
+        [SerializeField] private int m_depthToActivate = 3;
 
-        private void Awake() {
-            SceneManager.sceneLoaded += OnSceneLoaded;
-            m_loadedScenes[m_firstScene] = SceneManager.LoadSceneAsync(m_firstScene, LoadSceneMode.Additive);
-        }
-
-        private void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
-            Debug.Log("carreguei porra");
+        private void Start() {
+            // SetActiveMap(m_initialMap);
         }
         
-
+        public void AddMap(Map map) {
+            m_maps[map.Coordinates] = map;
+        }
+        
         public void SetActiveMap(Map map) {
-            var connections = MapConnectionConfig.Instance.GetConnections(map.Coordinates);
-            LoadConnections(connections);
+            m_maps.ForEach(kv => {
+                if (kv.Value != map) {
+                    kv.Value.Deactivate();    
+                }
+            });
+            map.Activate();
+            ActivateConnections(GetConnections(map.Coordinates), m_depthToActivate);
         }
 
-        private void LoadConnections(List<MapVertex> connections) {
+        private void ActivateConnections(List<Map> connections, int depth) {
+            if (depth <= 0) {
+                return;
+            }
             foreach (var connection in connections) {
-                if (!m_loadedScenes.ContainsKey(connection.scene)) {
-                    var async = SceneManager.LoadSceneAsync(connection.scene, LoadSceneMode.Additive);
-                    async.allowSceneActivation = true;
-                    m_loadedScenes[connection.scene] = async;
+                connection.Activate();
+                ActivateConnections(GetConnections(connection.Coordinates), --depth);
+            }
+        }
+
+        public List<Map> GetConnections(int x, int y) {
+            return GetConnections(new Vector2Int(x, y));
+        }
+
+        public List<Map> GetConnections(Vector2Int position) {
+
+            if (!m_maps.ContainsKey(position)) {
+                Debug.LogWarningFormat("Invalid position for GetConnections: {0}", position);
+                return new List<Map>();
+            }
+
+            var positions = new List<Vector2Int>
+            {
+                new Vector2Int(-1, 0), new Vector2Int(1, 0), new Vector2Int(0, 1), new Vector2Int(0, -1)
+                
+            };
+            var connections = new List<Map>();
+
+            for (int i = 0; i < positions.Count; i++) {
+                Vector2Int connectionPosition = position + positions[i];
+                if (m_maps.ContainsKey(connectionPosition)) {
+                    connections.Add(m_maps[connectionPosition]);
                 }
             }
+
+            return connections;
         }
     }
 }
