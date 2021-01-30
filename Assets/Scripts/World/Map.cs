16e949052c0363ba12498fec6fa2b7032050c855
Ricardo Cicaida Novak <ricardonovak@yahoo.com.br>
2021-01-30 01:32:54 -0300

    -Start MapManager implementation

--- Assets/Scripts/World/Map.cs
+++ Assets/Scripts/World/Map.cs
@@ -1,19 +1,26 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace World {
     public class Map : MonoBehaviour, ITiledMap {
+
+        [SerializeField] private List<string> m_adjacentScenes;
+        
+        public Vector3 MapCoordinates { get; private set; }
+        
         public void Setup(MapData data) {
-            Vector3 mapCoordinates = Vector3.zero;
+            MapCoordinates = Vector3.zero;
             foreach (var property in data.properties) {
                 if (property.name.Equals("mapx")) {
-                    mapCoordinates += new Vector3(int.Parse(property.value), 0);
+                    MapCoordinates += new Vector3(int.Parse(property.value), 0);
                 }
                 else if (property.name.Equals("mapy")) {
-                    mapCoordinates += new Vector3(0, int.Parse(property.value));
+                    MapCoordinates += new Vector3(0, int.Parse(property.value));
                 }
             }
-            mapCoordinates *= new Vector2(data.width, -data.height);
-            transform.position += mapCoordinates;
+            MapCoordinates *= new Vector2(data.width, -data.height);
+            transform.position += MapCoordinates;
         }
         
         public void OnEnterMap() {
