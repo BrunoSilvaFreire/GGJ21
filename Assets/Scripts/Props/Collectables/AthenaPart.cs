ca48e625b3a2c706dd468da4d288cdbc21e4946d
Ricardo Cicaida Novak <ricardonovak@yahoo.com.br>
2021-01-31 14:39:13 -0300

    -Improved athena parts speed

--- Assets/Scripts/Props/Collectables/AthenaPart.cs
+++ Assets/Scripts/Props/Collectables/AthenaPart.cs
@@ -6,18 +6,21 @@ using GGJ.Traits;
 using Lunari.Tsuki.Entities;
 using Props.Collectables;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace GGJ.Collectables {
     public class AthenaPart : Collectable, IPersistant {
 
         public int id;
         public bool follow = true;
+        private static Vector2 speedRange = new Vector2(0.05f, 0.15f);
         
         private bool m_savedActive;
         private Transform m_savedParent;
         private Vector3 m_savedPosition;
         private Collector m_savedCollector, m_collector;
         private PersistanceManager m_manager;
+        private float m_speed;
         
         protected override CollectionAction ProcessCollection(Entity entity) {
             if (entity.Access<Collector>(out var collector)) {
@@ -30,11 +33,12 @@ namespace GGJ.Collectables {
 
         private void Awake() {
             GameManager.Instance.RegisterAthenaPart(id);
+            m_speed = Random.Range(speedRange.x, speedRange.y);
         }
 
         private void FixedUpdate() {
             if (m_collector && follow) {
-                transform.position = Vector3.Lerp(transform.position, m_collector.Owner.transform.position, 0.1f);
+                transform.position = Vector3.Lerp(transform.position, m_collector.Owner.transform.position, m_speed);
             }
         }
 
