c9687d1e6a744f5e8911c08456f44c5655f64b8e
BrunoSilvaFreire <brunorbsf@gmail.com>
2021-02-02 18:45:40 -0300

    > Only open altar when grounded

--- Assets/Scripts/Master/Altar.cs
+++ Assets/Scripts/Master/Altar.cs
@@ -1,6 +1,7 @@
 using GGJ.Master.UI;
 using GGJ.Traits.Knowledge;
 using Lunari.Tsuki.Entities;
+using Movement;
 using Props.Interactables;
 using Sirenix.OdinInspector;
 using UnityEngine;
@@ -13,15 +14,20 @@ namespace GGJ.Master {
         private void OnEnable() {
             view.onShow.AddListener(OnShow);
         }
-        
+
         private void OnDisable() {
             view.onShow.RemoveListener(OnShow);
         }
-        
+
         [ShowInInspector]
         public override void Interact(Entity entity) {
             if (!entity.Access(out Knowledgeable _)) {
                 return;
+            }
+            if (entity.Access(out Motor motor)) {
+                if (!motor.supportState.down) {
+                    return;
+                }
             }
             var ui = PlayerUI.Instance;
             ui.KnowledgeEditor.Open();
