bc3e4630d072d8c1301df0e6f664176fd5892f24
BrunoSilvaFreire <brunorbsf@gmail.com>
2021-02-15 11:06:44 -0400

    > Render UI and Game to different textures

--- Assets/Scripts/UI/Common/PlayerUI.cs
+++ Assets/Scripts/UI/Common/PlayerUI.cs
@@ -1,9 +1,33 @@
+using System;
 using GGJ.UI.Common.Knowledge;
 using Lunari.Tsuki.Runtime.Singletons;
+using UnityEngine;
+using UnityEngine.UI;
 namespace GGJ.UI.Common {
+    [ExecuteInEditMode]
     public class PlayerUI : Singleton<PlayerUI> {
         public KnowledgeEditor KnowledgeEditor;
         public View deathCurtains;
-        private View last;
+        public RenderTexture gameTexture;
+        public RawImage uiImage;
+        public AspectRatioFitter uiRatio;
+
+        public RenderTexture UITexture => (RenderTexture)uiImage.texture;
+        private void Update() {
+            CheckUITexture();
+        }
+        private void CheckUITexture() {
+            var tex = UITexture;
+            var aspect = (float)gameTexture.width / gameTexture.height;
+            var height = Mathf.RoundToInt(uiImage.rectTransform.rect.height);
+            var targetWidth = Mathf.RoundToInt(height * aspect);
+            uiRatio.aspectRatio = aspect;
+            if (tex.width != targetWidth) {
+                tex.Release();
+                tex.height = height;
+                tex.width = (int)(height * aspect);
+                tex.Create();
+            }
+        }
     }
 }
